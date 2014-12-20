0f2a22fbc234eccab36c5ad08a1882b99de1dc29
Morteza Milani <mrtz.milani@googlemail.com>
2014-12-20 14:52:56 +0330

    Fix pdb downloader, ignore header of the datasets

--- scripts/pdbdownloader.sh
+++ scripts/pdbdownloader.sh
@@ -3,13 +3,40 @@
 INPUT_LIST="$1"
 OUTPUT_ROOT="$2"
 
-if [ ! -d $OUTPUT_ROOT ]; then
+while getopts ":h" opt; do
+  case $opt in
+    h)
+      echo "usage: pdbdownloader.sh dataset output_root_directory" >&2
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ ! -s "$INPUT_LIST" ]; then
+	echo "Dataset is empty or does not exists."
+	exit 1
+fi
+
+if [ "$OUTPUT_ROOT" == "" ]; then
+	echo "Output directory is not set."
+	exit 1
+fi
+
+if [ ! -d "$OUTPUT_ROOT" ]; then
   mkdir -p $OUTPUT_ROOT
 fi
 
-LIST=`cat $INPUT_LIST`
+# Read file lines into LIST array
+IFS=$'\n'
+set -f
+LIST=(`cat $INPUT_LIST -T`)
 
-for LINE in $LIST
+# Ignore header of the list and loop through the items
+for LINE in "${LIST[@]:1}"
 do
 	`cd $OUTPUT_ROOT && wget http://www.rcsb.org/pdb/files/${LINE:0:4}.pdb.gz`
 done
